c5bafa48dd07667007679da29411c216c2887170
Hoang Phuong <hphuong1503@outlook.com>
2022-10-20 20:40:37 +0700

    phuong

--- app/src/main/java/com/example/androidhello/MainActivity.java
+++ app/src/main/java/com/example/androidhello/MainActivity.java
@@ -12,6 +12,10 @@ import com.example.androidhello.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
 
+
+    /*
+    * Phuong code
+    * */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,67 +23,6 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.btnPlus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, PlusActivity.class);
-                startActivity(intent);
 
-
-            }
-        });
-
-
-
-
-
-        binding.btnMinus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MinusActivity.class);
-                startActivity(intent);
-            }
-
-
-        });
-
-        binding.btnSimpleEquation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SEquation_Activity.class);
-                startActivity(intent);
-
-
-            }
-        });
-
-        binding.btnQuadraticEquation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Quadratic.class);
-                startActivity(intent);
-
-
-            }
-        });
-
-        binding.btnGrades.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, GradesActivity.class);
-                startActivity(intent);
-
-
-            }
-        });
-        binding.btnMonth.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MonthActivity.class);
-                startActivity(intent);
-
-
-            }
-        });
     }
 }
